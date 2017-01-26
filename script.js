c09ccafef90f3dea328fe803573f545f0bd95b56
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2017-01-25 19:37:34 -0600

    add misc code

--- script.js
+++ script.js
@@ -1,63 +1,101 @@
 const CHART = document.getElementById('chart');
-//console.log(CHART);
+const RADAR = document.getElementById('radar');
+// console.log(RADAR);
 
+// Chart.defaults.global.responsive = false;
 
 let lineChart = new Chart(CHART, {
   type: 'line',
   data: {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
-        {
-            label: "My First dataset",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-        }
+      {
+        label: "My First dataset",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: "My Second dataset",
+        fill: true,
+        // lineTension: 0,
+        backgroundColor: "rgba(75,75,192,0.4)",
+        borderColor: "rgba(75,72,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,72,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,72,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [100, 20, 60, 20, 80, 55, 90],
+      }
     ]
-}
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          // beginAtZero: true,
+          max: 100,
+          min: 0,
+          stepSize: 10
+        }
+        }]
+    }
+  }
 });
 
 
-
-
-var data1 = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
+var myRadarChart = new Chart(RADAR, {
+  type: 'radar',
+  data:  {
+    labels: ["HTML", "CSS", "JavaScript", "JQuery", "Bootstrap", "Photoshop", "Misc"],
+    datasets: [
       {
-          label: "My First dataset",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
+        label: "Skill level",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        pointBackgroundColor: "rgba(255,99,132,1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(255,99,132,1)",
+        data: [70, 85, 65, 70, 85, 75, 70]
       }
-  ]
-}
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+          // reverse: true
+          // max: 100,
+          // min: 0,
+          // stepSize: 10
+        }
+        }]
+    }
+  }
+});
